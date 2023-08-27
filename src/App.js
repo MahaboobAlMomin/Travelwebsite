7ff339b2b5eb93e96a1072ccd3a5f6c9daea13a1
Mahaboob Alam Momin <86647030+MahaboobAlMomin@users.noreply.github.com>
2023-08-27 09:35:06 -0700

    app.js and app.css commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import Navbar  from "./Components/Navbar/Navbar.jsx";
+import Home from "./Components/Home/Home.jsx"
+import Main from "./Components/Main/Main.jsx"
+import Footer from "./Components/Footer/Footer.jsx"
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Main />
+      <Footer/>
+      
+    </>
+  )
+}
+
+export default App
